73cf8d1e483faef6328a046752abe62aa81fae1f
Rayyan <rayyankhalique@gmail.com>
2025-01-29 16:19:25 -0800

    working on weekly page ui

--- app/(tabs)/weekly.tsx
+++ app/(tabs)/weekly.tsx
@@ -6,48 +6,52 @@ import { DateTask, DefaultTask, WeekTask, Tasks, DaysOfWeek } from "@/utils/Data
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function WeeklyScreen() {
+  const [daySelected, setDaySelected] = useState<string>()
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}>
 
         <View style={styles.headerContainer}>
-          <Text style={styles.sectionTitle}>Weekly Tasks</Text>
+          <Text style={styles.headerSectionTitle}>Weekly Tasks</Text>
           
           <TouchableOpacity style={styles.addButton}>
-            <AntDesign name="plus" size={20} color='white'/>
+            <AntDesign name="plus" size={20} color='white' style={{marginLeft: 5}}/>
             <Text style={styles.btnText}>Add Task</Text>
           </TouchableOpacity>
         </View>
 
-        <View>
-          <TouchableOpacity>
-            <Text>Mo</Text>
+        <View style={styles.daysContainer}>
+          <TouchableOpacity onPress={() => setDaySelected('monday')} style={daySelected === "monday" ? [styles.dayBtn, {backgroundColor: 'yellow'}] : styles.dayBtn}>
+            <Text style={styles.dayBtnText}>Mo</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity>
-            <Text>Tu</Text>
+          <TouchableOpacity onPress={() => setDaySelected('tuesday')} style={daySelected === "tuesday" ? [styles.dayBtn, {backgroundColor: 'yellow'}] : styles.dayBtn}>
+            <Text style={styles.dayBtnText}>Tu</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity>
-            <Text>We</Text>
+          <TouchableOpacity onPress={() => setDaySelected('wednesday')} style={daySelected === "wednesday" ? [styles.dayBtn, {backgroundColor: 'yellow'}] : styles.dayBtn}>
+            <Text style={styles.dayBtnText}>We</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity>
-            <Text>Th</Text>
+          <TouchableOpacity onPress={() => setDaySelected('thursday')} style={daySelected === "thursday" ? [styles.dayBtn, {backgroundColor: 'yellow'}] : styles.dayBtn}>
+            <Text style={styles.dayBtnText}>Th</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity>
-            <Text>Fr</Text>
+          <TouchableOpacity onPress={() => setDaySelected('friday')} style={daySelected === "friday" ? [styles.dayBtn, {backgroundColor: 'yellow'}] : styles.dayBtn}>
+            <Text style={styles.dayBtnText}>Fr</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity>
-            <Text>Sa</Text>
+          <TouchableOpacity onPress={() => setDaySelected('saturday')} style={daySelected === "saturday" ? [styles.dayBtn, {backgroundColor: 'yellow'}] : styles.dayBtn}>
+            <Text style={styles.dayBtnText}>Sa</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity>
-            <Text>Su</Text>
+          <TouchableOpacity onPress={() => setDaySelected('sunday')} style={daySelected === "sunday" ? [styles.dayBtn, {backgroundColor: 'yellow'}] : styles.dayBtn}>
+            <Text style={styles.dayBtnText}>Su</Text>
           </TouchableOpacity>
         </View>
+      </View>
+      <View style={styles.tasksContainer}>
+        <Text>Tasks</Text>
       </View>
     </View>
   );
@@ -59,33 +63,49 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
   },
   topContainer: {
-    paddingTop: 90,
+    paddingTop: 80,
     paddingHorizontal: 30,
-    backgroundColor: '#28def3'
+    backgroundColor: '#999999'
   },
   headerContainer: {
     display: 'flex',
     justifyContent: 'space-between',
     flexDirection: 'row',
-    verticalAlign: 'middle'
+    verticalAlign: 'auto'
   },
-  sectionTitle: {
+  headerSectionTitle: {
     color: '#ffffff',
     fontWeight: 'bold',
-    fontSize: 28,
+    fontSize: 30,
   },
   addButton: {
     backgroundColor: '#9c2cf3',
-    width: 100,
-    height: 40,
+    width: 112.5,
+    height: 45,
     borderRadius: 45,
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-evenly',
     alignItems: 'center'
   },
   btnText: {
    color: '#fff',
    fontSize: 12,
    fontWeight: 'bold',
+   marginRight: 5
+  },
+  daysContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 25,
+    marginBottom: 15
+  },
+  dayBtn: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 30,
+    borderRadius: 45
+  },
+  dayBtnText: {
+    fontSize: 15
   }
 });
